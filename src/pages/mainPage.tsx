39ac20e466bd0d6216cb8704d5b4eeb11470d48d
mintcoke123 <weetee1014@gmail.com>
2025-08-08 21:57:50 +0900

    chore #3: 페이지 깃에 반영영

---
+++ src/pages/mainPage.tsx
@@ -0,0 +1,13 @@
+import Header from '../component/main/header';
+import KakaoMap from '../component/main/main/map';
+
+const MainPage = () => {
+  return (
+    <div>
+      <Header />
+      <KakaoMap />
+    </div>
+  );
+};
+
+export default MainPage;
