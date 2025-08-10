9ed96425c59d2301d46da1c83a30b6621804f1cd
INSANE-P <chanbin0626@gmail.com>
2025-08-11 02:58:50 +0900

    chore: Vite 환경에 맞춰 MSW 초기화 로직 수정 (#6)

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
-if (process.env.NODE_ENV === 'development') {
+if (import.meta.env.DEV) {
   const { worker } = await import('./mocks/browser');
-  worker.start();
+  await worker.start();
 }
 
 createRoot(document.getElementById('root')!).render(
