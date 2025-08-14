651067d9b558514ade79574798fecf4ace13e162
gxuoo <rbdud189@naver.com>
2025-08-14 21:27:18 +0900

    chore: 불필요한 줄바꿈 삭제

--- src/constants/register/index.ts
+++ src/constants/register/index.ts
@@ -5,37 +5,15 @@ export const REGISTER_PROCESS_STEPS = {
 };
 
 export const CATEGORY_GROUPS: CategoryGroup[] = [
-  {
-    category: '휴대폰',
-  },
-  {
-    category: '가방',
-  },
-  {
-    category: '결제 카드',
-  },
-  {
-    category: 'ID 카드',
-  },
-  {
-    category: '기숙사 카드',
-  },
-  {
-    category: '지갑',
-  },
-  {
-    category: '악세서리(금속)',
-  },
-  {
-    category: '패드',
-  },
-  {
-    category: '노트북',
-  },
-  {
-    category: '전자 음향기기',
-  },
-  {
-    category: '기타',
-  },
+  { category: '휴대폰' },
+  { category: '가방' },
+  { category: '결제 카드' },
+  { category: 'ID 카드' },
+  { category: '기숙사 카드' },
+  { category: '지갑' },
+  { category: '악세서리(금속)' },
+  { category: '패드' },
+  { category: '노트북' },
+  { category: '전자 음향기기' },
+  { category: '기타' },
 ];
