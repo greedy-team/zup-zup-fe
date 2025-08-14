4f6a5c459ea7979292fd854b0c41ba5396a0fe4b
gxuoo <rbdud189@naver.com>
2025-08-14 22:47:44 +0900

    refactor: 분실물 등록 Modal을 열기 위한 main 컴포넌트 로직 추가

--- src/component/main/main/main.tsx
+++ src/component/main/main/main.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import LostList from './lostList';
 import Map from './map';
+import RegisterModal from '../../register/RegisterModal';
 import type { Category } from '../../../types/main/category';
 import type { LostItem } from './lostListItem';
 
@@ -28,9 +30,12 @@ const Main = ({
 }: Props) => {
   const canSubmit = selectedLat != null && selectedLng != null;
 
+  // 등록 모달의 open, close를 관리하는 상태
+  const [isRegisterModalOpen, setIsRegisterModalOpen] = useState(false);
+
   return (
-    <main className="flex-1 min-h-0">
-      <div className="h-full min-h-0 grid grid-cols-[360px_1fr]">
+    <main className="min-h-0 flex-1">
+      <div className="grid h-full min-h-0 grid-cols-[360px_1fr]">
         <aside className="h-full overflow-y-auto border-r">
           <LostList items={items} selectedCategory={selectedCategory} selectedArea={selectedArea} />
         </aside>
@@ -43,15 +48,18 @@ const Main = ({
           />
           <button
             disabled={!canSubmit}
-            className="absolute right-5 bottom-5 z-10 rounded-full shadow-lg px-4 py-3 bg-emerald-600 text-white text-sm hover:bg-emerald-700  disabled:bg-gray-300 disabled:text-gray-600 disabled:cursor-not-allowed"
+            className="absolute right-5 bottom-5 z-10 rounded-full bg-teal-600 px-4 py-3 text-sm text-white shadow-lg hover:bg-teal-700 disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-600"
             onClick={() => {
-              setIsRegisterConfirmModalOpen(true);
+              setIsRegisterModalOpen(true);
             }}
           >
             분실물 추가
           </button>
         </section>
       </div>
+
+      {/* isRegisterModalOpen 상태에 따라 RegisterModal을 조건부 렌더링 */}
+      {isRegisterModalOpen && <RegisterModal onClose={() => setIsRegisterModalOpen(false)} />}
     </main>
   );
 };
