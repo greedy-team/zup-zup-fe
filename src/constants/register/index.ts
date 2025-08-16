09e85a2ca0690f5ac2d9902d981fcd0634c5ba2e
gxuoo <rbdud189@naver.com>
2025-08-17 05:53:27 +0900

    chore: mock 서버 연결 후, 사용하지 않는 상수 삭제

--- src/constants/register/index.ts
+++ src/constants/register/index.ts
@@ -1,19 +1,3 @@
-import type { CategoryGroup } from '../../types/register';
-
 export const REGISTER_PROCESS_STEPS = {
   STEPS: ['카테고리 선택', '상세 정보', '최종 확인'],
 };
-
-export const CATEGORY_GROUPS: CategoryGroup[] = [
-  { category: '휴대폰' },
-  { category: '가방' },
-  { category: '결제 카드' },
-  { category: 'ID 카드' },
-  { category: '기숙사 카드' },
-  { category: '지갑' },
-  { category: '악세서리(금속)' },
-  { category: '패드' },
-  { category: '노트북' },
-  { category: '전자 음향기기' },
-  { category: '기타' },
-];
