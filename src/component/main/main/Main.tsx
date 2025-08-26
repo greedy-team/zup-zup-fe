a282b14185dd2bc7acca6b1cc6eea83d6ea811d0
gxuoo <rbdud189@naver.com>
2025-08-26 15:18:53 +0900

    refactor: Context 사용에 따른 로직 변경

--- src/component/main/main/Main.tsx
+++ src/component/main/main/Main.tsx
@@ -1,32 +1,20 @@
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import Map from './Map';
 import LostList from './list/LostList';
 import { SelectedModeContext } from '../../../contexts/AppContexts';
+import { SelectedAreaIdContext } from '../../../contexts/AppContexts';
 
-const Main = ({
-  pagination,
-  mapSelection,
-  mode,
-  lists,
-  areas,
-  ui,
-}: MainComponentProps) => {
-  const [selectedItemForFind, setSelectedItemForFind] = useState<LostItemListItem | null>(null);
+const Main = () => {
   const navigate = useNavigate();
   const { selectedMode, setSelectedMode } = useContext(SelectedModeContext)!;
-
-  // 모드 토글 핸들러
-  const toggleMode = () => {
-    setSelectedMode(selectedMode === 'register' ? 'append' : 'register');
-  };
+  const { selectedAreaId } = useContext(SelectedAreaIdContext)!;
 
   const handleRegisterButtonClick = () => {
-    if (mode.selectedMode === 'register') {
-      mode.toggleMode();
+    if (selectedMode === 'register') {
+      setSelectedMode('append');
     } else {
-      navigate(`/register/${mapSelection.selectedAreaId}`);
+      navigate(`/register/${selectedAreaId}`);
     }
   };
 
@@ -43,7 +31,7 @@ const Main = ({
           <button
             className="absolute right-5 bottom-5 z-10 rounded-full bg-teal-600 px-4 py-3 text-sm text-white shadow-lg hover:bg-teal-700 disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-600"
             onClick={handleRegisterButtonClick}
-            disabled={mode.selectedMode !== 'register' && !mapSelection.selectedAreaId}
+            disabled={selectedMode !== 'register' && !selectedAreaId}
           >
             {selectedMode === 'register' ? '분실물 조회' : '분실물 추가'}
           </button>
