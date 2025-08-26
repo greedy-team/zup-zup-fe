54098288d49a84c3a20004d64ee53158e1225d9f
gxuoo <rbdud189@naver.com>
2025-08-26 15:18:02 +0900

    refactor: Header에 볼드체 적용 삭제

--- src/component/root/layout/Header.tsx
+++ src/component/root/layout/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
           반응형: (xs에서는 중앙 문구를 안보여 줌, sm부터 표시, 폰트의 크기도 단계적으로 변화)
           whitespace-nowrap: 줄 내 강제 줄바꿈을 못하게 하여 스타일을 유자
         */}
-        <p className="absolute top-1/2 left-1/2 hidden -translate-x-1/2 -translate-y-1/2 text-center text-lg leading-tight font-extrabold whitespace-nowrap sm:block sm:text-xl md:text-2xl lg:text-3xl">
+        <p className="absolute top-1/2 left-1/2 hidden -translate-x-1/2 -translate-y-1/2 text-center text-lg leading-tight whitespace-nowrap sm:block sm:text-xl md:text-2xl lg:text-3xl">
           {/* 첫번째 줄 검정색 폰트 */}
           <span className="block text-black">잃어버린 물건은?</span>
           {/* 둘번째 줄은 줍줍을 강조하기 위해서 웹사이트 색상 적용 */}
