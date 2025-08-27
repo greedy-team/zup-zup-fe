b6b6b48c22cb26869925185cdaa579ab4e9f4ef8
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2025-08-28 00:02:00 +0900

    style #64: 중복된 header 제거, 화면 높이 비율 100%로 변경

--- src/component/main/header/Header.tsx
+++ src/component/main/header/Header.tsx
@@ -9,12 +9,6 @@ const Header = () => {
 
   return (
     <header className="shrink-0 border-b">
-      <div className="flex h-30 items-center justify-between bg-teal-50 px-4 text-black">
-        <Logo />
-        <div className="text-3xl text-teal-600">잃어버린 물건은? 줍줍!</div>
-        <Authentication />
-      </div>
-
       <CategoryRadio />
       {selectedMode === 'register' && (
         <div className="absolute top-0 right-0 left-0 z-40 h-30 bg-gray-500/30" />
