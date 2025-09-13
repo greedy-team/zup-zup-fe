4dbf7c2ab6f1e41deb2bd2c61fad8650e571a160
INSANE-P <chanbin0626@gmail.com>
2025-09-13 19:13:34 +0900

    chore: Flat Config 정리로 설정 에러 해결 (#94)

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,13 @@
+import js from '@eslint/js';
 import globals from 'globals';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
 
+import reactPlugin from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import importPlugin from 'eslint-plugin-import';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+
+import tsParser from '@typescript-eslint/parser';
 import { FlatCompat } from '@eslint/eslintrc';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
@@ -12,38 +18,123 @@ const compat = new FlatCompat({
   resolvePluginsRelativeTo: __dirname,
 });
 
+const airbnbJs = compat.extends('airbnb', 'airbnb/hooks').map((cfg) => ({
+  ...cfg,
+  files: ['**/*.{js,jsx}'],
+}));
+
+const tsAirbnb = compat.extends('airbnb-typescript').map((cfg) => ({
+  ...cfg,
+  files: ['**/*.{ts,tsx}'],
+}));
+
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules', 'public/mockServiceWorker.js', 'eslint.config.js'] }
+  {
+    plugins: {
+      react: reactPlugin,
+      import: importPlugin,
+      'jsx-a11y': jsxA11yPlugin,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+  },
 
-  ...compat.extends('airbnb', 'airbnb/hooks', 'airbnb-typescript'),
-
+  ...airbnbJs,
   js.configs.recommended,
-  ...tseslint.configs.recommended,
-  reactHooks.configs['recommended-latest'],
-  reactRefresh.configs.vite,
+  ...tsAirbnb,
 
   {
     files: ['**/*.{ts,tsx}'],
-
     languageOptions: {
-      ecmaVersion: 'latest',
+      ecmaVersion: 2022,
       sourceType: 'module',
       globals: globals.browser,
-      parserOptions: { project: './tsconfig.json' },
+      parser: tsParser,
+      parserOptions: { project: ['./tsconfig.app.json'], tsconfigRootDir: __dirname },
     },
-    settings: {
-      'import/resolver': { typescript: { project: ['./tsconfig.json'] } },
+    settings: { 'import/resolver': { typescript: { project: ['./tsconfig.app.json'] } } },
+    rules: {
+      'react-hooks/rules-of-hooks': 'off',
+      '@typescript-eslint/indent': 'off',
+      'no-empty': 'off',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-refresh/only-export-components': 'warn',
+
+      'react/jsx-filename-extension': 'off',
+      'react/function-component-definition': 'off',
+      'react/jsx-one-expression-per-line': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'react/button-has-type': 'off',
+      'react/self-closing-comp': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+
+      'import/prefer-default-export': 'off',
+      'import/extensions': 'off',
+      'import/newline-after-import': 'off',
+      'import/order': 'off',
+      'import/no-duplicates': 'off',
+      'import/no-useless-path-segments': 'off',
+
+      'object-curly-newline': 'off',
+      'operator-linebreak': 'off',
+      'implicit-arrow-linebreak': 'off',
+      'function-paren-newline': 'off',
+      'spaced-comment': 'off',
+
+      'max-len': [
+        'warn',
+        { code: 120, ignoreStrings: true, ignoreTemplateLiterals: true, ignoreComments: true },
+      ],
+      'no-else-return': 'off',
+      curly: 'off',
+      'no-plusplus': 'off',
+      'no-restricted-syntax': 'off',
+      'no-console': 'off',
+
+      '@typescript-eslint/no-shadow': 'off',
+      '@typescript-eslint/no-throw-literal': 'off',
+      '@typescript-eslint/naming-convention': 'off',
+
+      'jsx-a11y/label-has-associated-control': 'off',
+    },
+  },
+  {
+    files: [
+      'src/tests/**/*',
+      'src/mocks/**/*',
+      '**/*.test.{ts,tsx}',
+      '**/*.spec.{ts,tsx}',
+      'src/tests/setup.ts',
+    ],
+    rules: {
+      'import/no-extraneous-dependencies': 'off',
+      'global-require': 'off',
+      'import/first': 'off',
+    },
+  },
+
+  {
+    files: ['**/*.d.ts'],
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'no-undef': 'off',
+      '@typescript-eslint/naming-convention': 'off',
+    },
+  },
+  {
+    files: ['vite.config.ts', 'vitest.config.ts', 'tailwind.config.*', 'postcss.config.*'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: globals.node,
+      parser: tsParser,
+      parserOptions: { project: ['./tsconfig.node.json'], tsconfigRootDir: __dirname },
     },
     rules: {
-      'import/no-relative-parent-imports': 'error',
-      'prefer-destructuring': ['error', { object: true, array: true }],
-      'no-restricted-syntax': [
-        'error',
-        { selector: 'ForInStatement', message: 'for..in 금지' },
-        { selector: 'LabeledStatement', message: 'label 금지' },
-        { selector: 'WithStatement', message: 'with 금지' },
-      ],
-      'arrow-body-style': 'off',
+      'import/no-extraneous-dependencies': 'off',
     },
   },
 ];
