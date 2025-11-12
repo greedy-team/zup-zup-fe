720e9af79069a6b6083260639a622e7b49dac213
gxuoo <rbdud189@naver.com>
2025-11-13 01:38:40 +0900

    feat: 로그인/로그아웃 버튼의 문구 추가 및 반응형 디자인 추가

--- src/component/root/layout/Authentication.tsx
+++ src/component/root/layout/Authentication.tsx
@@ -24,12 +24,34 @@ export default function Authentication() {
   return (
     <div className="flex aspect-square flex-shrink-0 flex-col items-center justify-center">
       {!isAuthenticated ? (
-        <button type="button" onClick={goLoginPage}>
-          <img src={LoginIcon} alt="login" className="h-8 w-8 cursor-pointer" />
+        <button
+          type="button"
+          onClick={goLoginPage}
+          className="flex h-full w-full cursor-pointer flex-col items-center justify-center"
+        >
+          <img
+            src={LoginIcon}
+            alt="login"
+            className="h-6 w-6 cursor-pointer sm:h-15 sm:w-15 md:h-8 md:w-8"
+          />
+          <span className="text-xs text-gray-600 group-hover:text-teal-500 sm:text-lg md:text-sm">
+            로그인
+          </span>
         </button>
       ) : (
-        <button type="button" onClick={handleLogout}>
-          <img src={LogoutIcon} alt="logout" className="h-8 w-8 cursor-pointer" />
+        <button
+          type="button"
+          onClick={handleLogout}
+          className="flex h-full w-full cursor-pointer flex-col items-center justify-center"
+        >
+          <img
+            src={LogoutIcon}
+            alt="logout"
+            className="h-6 w-6 cursor-pointer sm:h-15 sm:w-15 md:h-8 md:w-8"
+          />
+          <span className="text-xs text-gray-600 group-hover:text-teal-500 sm:text-lg md:text-sm">
+            로그아웃
+          </span>
         </button>
       )}
     </div>
