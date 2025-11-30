3228e468496f9403f50002e29172ac94640779d4
gxuoo <rbdud189@naver.com>
2025-11-30 14:04:31 +0900

    refactor: 공통 클래스에 그림자 요소 제거, input 태그의 active 상태 제거

--- src/constants/common/index.ts
+++ src/constants/common/index.ts
@@ -1,5 +1,5 @@
 export const COMMON_BUTTON_CLASSNAME =
-  'cursor-pointer rounded-lg shadow-md transition focus-visible:ring-2 focus-visible:outline-none active:translate-y-[1px]';
+  'cursor-pointer rounded-lg transition focus-visible:ring-2 focus-visible:outline-none active:translate-y-[1px]';
 
 export const COMMON_INPUT_CLASSNAME =
-  'rounded-lg shadow-md transition focus-visible:ring-2 focus-visible:outline-none active:translate-y-[1px]';
+  'rounded-lg transition focus-visible:ring-2 focus-visible:outline-none';
