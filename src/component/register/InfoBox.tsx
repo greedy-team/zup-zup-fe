ac37da13841fa710051fb63889ab15d2706d4bbe
임규영 <rbdud189@naver.com>
2026-04-06 00:34:24 +0900

    refactor #211 : 보관 장소 안내 문구 추가 및 등록 폼 스타일 개선

--- src/component/register/InfoBox.tsx
+++ src/component/register/InfoBox.tsx
@@ -4,7 +4,7 @@ const InfoBox: React.FC<{ title: string; children: React.ReactNode; className?: 
   className,
 }) => (
   <div className={`rounded-lg bg-gray-100 p-4 ${className}`}>
-    <h4 className="mb-2 text-xl font-medium text-gray-500">{title}</h4>
+    <h4 className="mb-2 text-lg font-medium text-gray-500">{title}</h4>
     <div className="text-gray-800">{children}</div>
   </div>
 );
